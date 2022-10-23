d1930b9a2189547f23763bd4f3c31eb0ea67dc35
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-10-24 01:19:21 +0300

    Finally

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
-
-
+class Book
+    attr_reader :title
+    attr_accessor :page_count, :author, :genre
+    def initialize(title)
+        @title = title 
+    end
+    def turn_page
+        # puts "can turn the page"
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+book = Book.new('dennis')
+puts book.title
+book_au  = Book.new('Tu stories twa denno')
+puts book_au.author
